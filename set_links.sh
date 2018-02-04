11f79c04785dccb21e8459a330207a69148c4c02
James Eckersall <james.eckersall@gmail.com>
2018-02-04 10:10:47 +0000

    set links script

--- set_links.sh
+++ set_links.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-HOME=~/tmp2
+HOME=~
 
 ln -fvs $DIR/bashrc $HOME/.bashrc
 ln -fvs $DIR/gitconfig $HOME/.gitconfig
 ln -fvs $DIR/git-prompt.sh $HOME/.git-prompt.sh
 ln -fvs $DIR/tmux.conf $HOME/.tmux.conf
-ln -fvs $DIR/vim $HOME/.vim
+ln -fvs $DIR/vim/update_bundles $HOME/.vim/update_bundles
 ln -fvs $DIR/vimrc $HOME/.vimrc
